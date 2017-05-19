07c3961163e58b762e84446c383aa7bdbff3e5cd
barryirvine <barry@we7.com>
2017-05-19 11:16:17 +0100

    Initial commit.
    Basic empty activity and the libraries that I'm likely to be using.

---
+++ app/src/main/java/com/barryirvine/shazam/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.barryirvine.shazam.ui.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.barryirvine.shazam.R;
+import com.barryirvine.shazam.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
